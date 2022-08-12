0935cf81eefa11f8fe7be72708b7935320fb5863
José Orlando de Queiroz <js.orlando.queiroz@gmail.com>
2022-08-11 21:07:56 -0300

    Set up project initial organization and metadata

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dundie',
+    version='0.1.0',
+    description='Reward Point System for Dunder Mifflin',
+    author='José Orlando de Queiroz',
+    packages=find_packages(),
+)
